87feb28935e0d6b44489bb0af8818ee79e8b67fa
A Ogudv <Rune777@yandex.ru>
2020-02-04 12:39:49 +0700

    rewrite test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    page.open()                      # открываем страницу
+    page.go_to_login_page()
