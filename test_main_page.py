22213303d6d0737c118ea0e5e0b22e02b5706b47
A Ogudv <Rune777@yandex.ru>
2020-02-04 13:23:46 +0700

    add login_page and add test for form in login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,13 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
-    page.open()                      # открываем страницу
-    page.go_to_login_page()
+    page.open()         # открываем страницу
     page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
